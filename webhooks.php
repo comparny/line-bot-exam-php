c1d06f408f128aec913f8bfa0ea6d0d183dbd9df
comparny <54089760+comparny@users.noreply.github.com>
2019-08-14 11:29:20 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ykmQl/VehCua9FW6o7lMnL+RKcRshu2GJ8r9NVLlU0DdgBw+g06OG4KW3ll80gKZHJMVAcXhvlX11o7mZ+JFNP+/0TsuwSmPX6gjzVDfOpryQ8BpMyN1a6RKjq4mhEWeoceK4Qhk+z4vynoePOZ8EwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
